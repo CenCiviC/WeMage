98edac0ef1d88b40fcfb3e759f9f48edd2b1fc66
CenCiviC <rudqsl513@gmail.com>
2023-05-24 18:26:21 +0900

    Initial Commit

---
+++ MCCamera/MCCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCCameraApp.swift
+//  MCCamera
+//
+//  Created by kyungbin on 2023/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MCCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
